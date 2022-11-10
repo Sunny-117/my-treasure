44754a3c7a9030a7b14b8cddd3065dc1e3634199
付志强 <zhiqiangfu6@gmail.com>
2022-11-10 21:38:39 +0800

    feat: add new feature

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    // css预处理器
-    preprocessorOptions: {
-      less: {
-        charset: false,
-        // additionalData: '@import "./src/assets/style/global.less";',
-      },
-    },
-  },
   server: {
     proxy: {
       '/api': {
