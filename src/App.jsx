cac35fb3ccdee13ec92c2677c9deb7b6b797d453
付志强 <zhiqiangfu6@gmail.com>
2022-11-10 20:12:49 +0800

    feat: add new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './components/Login'
+import Login from './pages/login'
 
 export default function App() {
   return (
