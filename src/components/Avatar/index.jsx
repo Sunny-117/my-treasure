a26d31c9b4b8ffd5f73abfe947cf618a7c79b13a
付志强 <zhiqiangfu6@gmail.com>
2022-11-10 21:47:49 +0800

    feat: add new feature

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 
-export default function Avatar({ url }) {
+export default function Avatar({ url, radius }) {
+    console.log(radius)
 
     return (
-        <div className='avatar'>
+        <div className='avatar' style={{
+            borderRadius: radius
+        }}>
             <img src={url} />
         </div>
     )
