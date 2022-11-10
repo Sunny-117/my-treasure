fa1b24159b095a1622f1b66aae47fa09732a4f11
付志强 <zhiqiangfu6@gmail.com>
2022-11-10 20:15:19 +0800

    feat: add new feature

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,7 +3,9 @@ import Login from '../../components/Login/index'
 export default function LoginPage() {
     return (
         <div style={{
+            // height: '100vh',
             display: 'flex',
+            border: '1px solid',
             justifyContent: 'center',
             alignItems: 'center'
         }}>
