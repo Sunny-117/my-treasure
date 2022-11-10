2b178e6ced2deecf2a9e6757f0bcd2d1bd0da393
付志强 <zhiqiangfu6@gmail.com>
2022-11-10 21:55:19 +0800

    feat: add new feature

--- src/components/ChatInput/index.jsx
+++ src/components/ChatInput/index.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-
+import './index.less'
 export default function ChatInput() {
     const [value, setValue] = useState('')
     return (
-        <div>
+        <div className='chat-input'>
             <input type="text" value={value} onChange={e => {
                 setValue(e.target.value)
             }} />
