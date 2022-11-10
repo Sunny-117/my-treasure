326088b96826c58adc67d43929910fbcd82babed
付志强 <zhiqiangfu6@gmail.com>
2022-11-10 21:05:19 +0800

    feat: add new feature

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 const instance = axios.create({
     baseURL: 'https://open.duyiedu.com',
     timeout: 1000,
-    headers: { 'Content-Type': 'application/json' }
+    // headers: { 'Content-Type': 'application/json' }
 });
 export default instance
