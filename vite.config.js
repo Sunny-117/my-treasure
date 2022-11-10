1f9ee49532deabf4a08eb0386057bae96ef75805
付志强 <zhiqiangfu6@gmail.com>
2022-11-10 21:03:39 +0800

    feat: add new feature

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:5173/',
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, "src"),
