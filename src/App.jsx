831f266ed0f903331d722260133cae562e27dc1d
付志强 <zhiqiangfu6@gmail.com>
2022-11-10 20:08:39 +0800

    feat: add new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Register'
+
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="/register" element={<Register></Register>}></Route>
+        </Routes>
+      </Router>
+    </div>
   )
 }
