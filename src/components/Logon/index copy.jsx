438556cfefda9b2cd92ed11ebcad436cbd937e9b
付志强 <zhiqiangfu6@gmail.com>
2022-11-10 21:31:09 +0800

    feat: add new feature

---
+++ src/components/Logon/index copy.jsx
@@ -0,0 +1,93 @@
+
+
+import { Button, Checkbox, Form, Input } from 'antd';
+import React from 'react';
+import instance from '@/api/index'
+
+const Logon = () => {
+    const onFinish = async (values) => {
+        const res = await instance.post('/api/user/reg', {
+            "loginId": "sunny",
+            "nickname": "sunny",
+            "loginPwd": "qwert123"
+            // appkey: "__sunny___1615100707839",
+            // account: "sunny",
+            // username: 'sunny',
+            // password: "aa1111",
+            // rePassword: "aa1111"
+        })
+        console.log(res)
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+    return (
+        <Form
+            name="basic"
+            labelCol={{
+                span: 8,
+            }}
+            wrapperCol={{
+                span: 16,
+            }}
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your username!',
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your password!',
+                    },
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+                label="Repassword"
+                name="Repassword"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Repassword!',
+                    },
+                ]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+
+            <Form.Item
+                wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                }}
+            >
+                <Button type="primary" htmlType="submit">
+                    注册
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+export default Logon;
