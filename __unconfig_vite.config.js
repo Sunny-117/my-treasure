d1fdc2a47f175433310fd6eb36f5c8a997a0b010
付志强 <zhiqiangfu6@gmail.com>
2022-12-08 14:41:41 +0800

    chore: init

--- __unconfig_vite.config.js
+++ __unconfig_vite.config.js
@@ -1,10 +1,14 @@
+
+let __unconfig_data;
+let __unconfig_stub = function (data = {}) { __unconfig_data = data };
+__unconfig_stub.default = (data = {}) => { __unconfig_data = data };
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { ohmytsVite } from '@ohmyts/vite'
 const r = (p) => resolve(__dirname, p)
 // https://vitejs.dev/config/
-export default defineConfig({
+const __unconfig_default =  defineConfig({
   server: {
     proxy: {
       '/api': {
@@ -27,3 +31,5 @@ export default defineConfig({
   }),]
 })
 
+
+if (typeof __unconfig_default === "function") __unconfig_default(...[{"command":"serve","mode":"development"}]);export default __unconfig_data;
